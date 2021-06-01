3e1669633ecbdeb01daaeaa26414fe12d22b5a25
yufuchang <yufuchang@inspur.com>
2021-06-01 19:40:45 +0800

    项目3生成https证书提交

--- src/main/java/com/yufuchang/Springboot3TomcatHttpsApplication.java
+++ src/main/java/com/yufuchang/Springboot3TomcatHttpsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot3TomcatHttpsApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3TomcatHttpsApplication.class, args);
 
